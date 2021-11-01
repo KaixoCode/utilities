8ffaa2fa16654c8ba881c6d1d6189f61d960a861
KaixoCode <jeroenzwiers2@gmail.com>
2021-11-01 20:25:53 +0100

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -750,6 +750,11 @@ struct callable {
 
 #include "list_comprehension.hpp"
 
+
+
+
+
+
 int main()
 {
 
@@ -772,48 +777,55 @@ int main()
 
         // Simple list comprehension with ranges and a constraint.
         var<int> a, b, c;
-        auto r1 = lc[(a, b, c) | c <- range(1, 11), b <- range(1, c), a <- range(1, b), a* a + b * b == c * c];
-        
+        auto r1 = lc[(a, b, c) | c <- range(1, 11), b <- range(1, c), a <- range(1, b), a*a + b*b == c*c];
+
         // Parallel iteration
         var<std::tuple<int, int>> d;
         auto r2 = lc[d | d <- (range(0, 10), range(0, 10))];
-        
+    
         // Iteration on some container
         std::vector<int> data{ 1, 2, 3, 4, 5 };
         auto r3 = lc[a + b | a <- data, b <- range(0, 10)];
-        
+
         // Determine resulting container
         std::vector<std::string> strings{ "hello", "carrot", "pizza" };
         var<std::tuple<std::string, int>> e;
-        auto r4 = lc[map(e.get<0>(), e.get<1>()) | e <- (strings, range(0, 100))];
-        
-        // Call std functions (need to call to<int>() because max returns const ref, which can't be stored in std::vector)
+        auto r4 = lc[map(e.get<0>(), e.get<1>()) | e <- (strings, range(0, 100))]; 
+    
+        // Call std functions
         std::vector<int> ints1{ 5, 2, 7, 3, 1, 9 };
         std::vector<int> ints2{ 4, 1, 8, 9, 3, 2 };
         var<std::tuple<int, int>> f;
         auto r5 = lc[max(f.get<0>(), f.get<1>()) | f <- (ints1, ints2)];
-        
+    
         // Automatically uses std::string when working with characters (to<char>() because tolower() returns int)
         std::string mystr = "HelloWorld";
         var<char> g;
         std::string r6 = lc[tolower(g).to<char>() | g <- mystr];
+    
+        // Nested list comprehension for lists of lists
+        var<int> x;
+        var<std::vector<int>> xs;
+        std::vector<std::vector<int>> xxs{ { 1,3,5,2,3,1,2,4,5 }, { 1,2,3,4,5,6,7,8,9 }, { 1,2,4,2,1,6,3,1,3,2,3,6 } };
+        auto r8 = lc[lcv[x | x <- xs, x % 2 == 0] | xs <- xxs];
+    
+        // Using a used variable as a container
+        auto r9 = lc[x | xs <- xxs, x <- xs];
+    
+        // Make a utility function
+        auto indices = [x = var<std::tuple<int, int>>{}](auto& data, const var<int>& a) mutable {
+            // Parallel iteration of value and index, constraint on value == argument, store index.
+            return lc[x.get<1>() | x <- (data, range(0, data.size())), a == x.get<0>()];
+        };
+
+        std::vector<int> datas{ 0, 1, 1, 0, 0, 1, 0 };
+        auto r12 = indices(datas, 1);
+
+        [x = var<int>{}] () mutable {
+            return lc[x | x <- range(0, 10)];
+        };
+
         
-
-        auto factors = [x = var<int>{}](const var<int>& n) mutable {
-            return lcv[x | x <- range(1, n + 1), ((n % x) == 0)]; 
-        };
-
-        auto prime = [&](const var<int>& n) { return factors(n).size() == 2; };
-
-        auto primes = [&, x = var<int>{}](const var<int>& n) mutable {
-            return lc[x | x <- range(2, n + 1), prime(x)];
-        };
-
-        var<int> q; 
-
-        auto pr = primes(13);
-
-
         std::cout << "";
     }
 
