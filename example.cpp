d003e0fc97bf4d355979f85974c2f76121f33434
KaixoCode <jeroenzwiers2@gmail.com>
2022-08-08 00:40:21 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -10,44 +10,6 @@ using namespace kaixo;
 template<class Ty, class ...Tys>
 concept OneOf = (std::same_as<Ty, Tys> || ...);
 
-class Json {
-public:
-    using Floating = double;
-    using Integral = int64_t;
-    using Unsigned = uint64_t;
-    using String = std::string;
-    using Boolean = bool;
-    using Array = std::vector<Json>;
-    using Object = std::map<String, Json, std::less<void>>;
-    using Null = std::nullptr_t;
-
-private:
-    using JsonValue = std::variant<Floating, Integral, Unsigned, String, Boolean, Array, Object, Null>;
-    struct Type { enum { Floating, Integral, Unsigned, String, Boolean, Array, Object, Null }; };
-    JsonValue m_Value;
-
-    template<class Ty> struct Alias { using Type = Ty; };
-    template<std::signed_integral Ty> struct Alias<Ty> { using Type = Integral; };
-    template<std::unsigned_integral Ty> struct Alias<Ty> { using Type = Unsigned; };
-
-public:
-    template<class Ty = Null>
-    Json(const Ty& ty = {}) : m_Value(static_cast<Alias<Ty>::Type>(ty)) {}
-
-    template<class Ty> Ty get() const { return static_cast<Ty>(std::get<typename Alias<Ty>::Type>(m_Value)); }
-    template<class Ty> Ty& ref() { return std::get<Ty>(m_Value); }
-    template<class Ty> const Ty& ref() const { return std::get<Ty>(m_Value); }
-
-    Json& operator[](std::string_view index)
-    {
-        if (m_Value.index() == Type::Null) m_Value = Object{};
-        else if (m_Value.index() != Type::Object) throw std::exception("Not an object.");
-        auto _it = ref<Object>().find(index);
-        if (_it == ref<Object>().end()) return ref<Object>()[std::string{ index }];
-        else return _it->second;
-    }
-};
-
 template<class Ty> concept attribute = requires(Ty) { typename Ty::is_attribute; };
 template<class Ty> struct is_attribute_impl { constexpr static bool value = attribute<Ty>; };
 
@@ -133,46 +95,81 @@ template<class Ty> struct is_json_property_attribute_impl { constexpr static boo
 constexpr type_trait<is_json_property_attribute_impl> is_json_property{};
 
 #define JsonPropertyName(NAME) no_unique_address]] json_property<NAME> KAIXO_MERGE(_json_property, __COUNTER__){}; [[
+class Json {
+public:
+    using Floating = double;
+    using Integral = int64_t;
+    using Unsigned = uint64_t;
+    using String = std::string;
+    using Boolean = bool;
+    using Array = std::vector<Json>;
+    using Object = std::map<String, Json, std::less<void>>;
+    using Null = std::nullptr_t;
 
-template<aggregate Ty>
-Json to_json(const Ty& value) {
-    static const auto member_pointers = get_member_ptrs<decay_t<Ty>>();
-    using members = attributes_from_struct_t<decay_t<Ty>>;
-    
-    Json _result{};
-    indexed_for<members::size>([&]<std::size_t I>{
-        using member = members::template member<I>;
-        using type = member::type;
-        using attributes = member::attributes;
-        using find_json_property = attributes::template filter<is_json_property>;
-        if constexpr (find_json_property::size != 0) {
-            constexpr std::string_view name = find_json_property::type::name;
-            _result[name] = (value.*std::get<I>(member_pointers));
-        }
-    });
-    return _result;
-}
+private:
+    using JsonValue = std::variant<Floating, Integral, Unsigned, String, Boolean, Array, Object, Null>;
+    struct Type { enum { Floating, Integral, Unsigned, String, Boolean, Array, Object, Null }; };
+    JsonValue m_Value;
 
-template<aggregate Ty>
-Ty from_json(Json& json) {
-    static const auto member_pointers = get_member_ptrs<decay_t<Ty>>();
-    using members = attributes_from_struct_t<decay_t<Ty>>;
-    
-    Ty _result{};
-    indexed_for<members::size>([&]<std::size_t I>{
-        using member = members::template member<I>;
-        using type = member::type;
-        using attributes = member::attributes;
-        using find_json_property = attributes::template filter<is_json_property>;
-        if constexpr (find_json_property::size != 0) {
-            constexpr std::string_view name = find_json_property::type::name;
-            (_result.*std::get<I>(member_pointers)) = json[name].get<type>();
-        }
-    });
-    return _result;
-}
+    template<class Ty> struct Alias { using Type = Ty; };
+    template<std::signed_integral Ty> struct Alias<Ty> { using Type = Integral; };
+    template<std::unsigned_integral Ty> struct Alias<Ty> { using Type = Unsigned; };
+
+public:
+    template<class Ty = Null>
+    Json(const Ty& ty = {}) : m_Value(static_cast<Alias<Ty>::Type>(ty)) {}
+
+    template<aggregate Ty>
+    Json(const Ty& value) : m_Value(Object{}) {
+        static const auto member_pointers = get_member_ptrs<decay_t<Ty>>();
+        using members = attributes_from_struct_t<decay_t<Ty>>;
+
+        indexed_for<members::size>([&]<std::size_t I>{
+            using member = members::template member<I>;
+            using type = member::type;
+            using attributes = member::attributes;
+            using find_json_property = attributes::template filter<is_json_property>;
+            if constexpr (find_json_property::size != 0) {
+                constexpr std::string_view name = find_json_property::template element<0>::type::name;
+                (*this)[name] = (value.*std::get<I>(member_pointers));
+            }
+        });
+    }
+
+    template<class Ty> Ty get() const { return static_cast<Ty>(std::get<typename Alias<Ty>::Type>(m_Value)); }
+    template<class Ty> Ty& ref() { return std::get<Ty>(m_Value); }
+    template<class Ty> const Ty& ref() const { return std::get<Ty>(m_Value); }
+    template<aggregate Ty> Ty get() { 
+        static const auto member_pointers = get_member_ptrs<decay_t<Ty>>();
+        using members = attributes_from_struct_t<decay_t<Ty>>;
+
+        Ty _result{};
+        indexed_for<members::size>([&]<std::size_t I>{
+            using member = members::template member<I>;
+            using type = member::type;
+            using attributes = member::attributes;
+            using find_json_property = attributes::template filter<is_json_property>;
+            if constexpr (find_json_property::size != 0) {
+                constexpr std::string_view name = find_json_property::template element<0>::type::name;
+                (_result.*std::get<I>(member_pointers)) = (*this)[name].get<type>();
+            }
+        });
+        return _result;
+    }
+
+    Json& operator[](std::string_view index)
+    {
+        if (m_Value.index() == Type::Null) m_Value = Object{};
+        else if (m_Value.index() != Type::Object) throw std::exception("Not an object.");
+        auto _it = ref<Object>().find(index);
+        if (_it == ref<Object>().end()) return ref<Object>()[std::string{ index }];
+        else return _it->second;
+    }
+};
+
 
 struct Person {
+    [[JsonPropertyName("age")]]
     [[JsonPropertyName("age")]]
     int age;
 
@@ -186,16 +183,44 @@ struct Person {
     double height;
 };
 
+struct Struct1 {
+    [[JsonPropertyName("value")]]
+    int value;
+
+    [[JsonPropertyName("carrot")]]
+    double carrot;
+};
+
+struct Struct2 {
+    [[JsonPropertyName("nested")]]
+    Struct1 nested;
+
+    [[JsonPropertyName("value")]]
+    std::string value;
+};
+
 int main() {
 
-    Person _person{
-        .age = 19,
-        .name = "Jimmy",
-        .height = 167,
+    Struct2 _s2{
+        .nested {
+            .value = 1,
+            .carrot = 2.2,
+        },
+        .value = "soup"
     };
 
-    Json _json = to_json(_person);
-    Person _value = from_json<Person>(_json);
+    Json _json = _s2;
+
+
+
+
+
+
+
+
+
+
+    Struct2 _value = _json.get<Struct2>();
 
     return 0;
 }
