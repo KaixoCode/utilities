46a84ddf85d932dda276eec950f0c839d979e600
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-23 15:58:04 +0200

    a

--- example.cpp
+++ example.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
-
-
+#include <set>
+#include <deque>
+#include <map>
 
 #include "kaixo/type_utils.hpp"
 #include "kaixo/expression.hpp"
@@ -16,13 +17,40 @@ using namespace kaixo::default_variables;
 
 int main() {
 
+    static_assert(
+        same_as<
+            info<
+                std::tuple<int, double>,
+                std::vector<double>,
+                std::unordered_map<std::string, int>,
+                std::list<long long>,
+                std::set<float>,
+                std::vector<std::string_view>,
+                std::map<int, double>,
+                std::deque<char>,
+                std::array<std::string, 4>
+            >
 
-    info<int, double, int>
-        ::transform<std::vector>
-        ::tparams
-        ::transform<drop_last<1>::type>
-        ::transform<grab::type>
-        ::bytes::size;
+            ::filter<has::value_type && with<grab::value_type>(is_arithmetic)>
+
+            ::when<with<grab::value_type>(is_integral)>
+                ::transform<reinstantiate<int>::type>
+
+            ::when<with<grab::value_type>(is_floating_point)>
+                ::transform<reinstantiate<double>::type>
+            
+            ::sort<type_sorters::size>
+        
+            ::as<std::tuple>,
+
+            std::tuple<
+                std::set<double>,
+                std::list<int>,
+                std::vector<double>,
+                std::deque<int>
+            >
+        >
+    );
 
     return 0;
 }
