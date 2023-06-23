5b91d68496946d60848eedd2a9fa253249d0ea5f
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-23 16:53:03 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -14,8 +14,34 @@ using namespace kaixo::operators;
 using namespace kaixo::overloads;
 using namespace kaixo::default_variables;
 
+template<require<(is_class || is_array) && !is_void> Ty>
+struct my_struct {
+
+};
+
+
+template<class ...Args>
+constexpr auto my_fun(Args&&...args) {
+    template_pack<Args...> pack{ args... };
+    auto last = pack | last_v<1> | get_v<0>;
+    auto rest = pack | drop_last_v<1>;
+
+    auto sum_rest = rest | call_v([]<class ...Tys>(Tys&& ...args) {
+        return (args + ...);
+    });
+
+    return sum_rest * last;
+}
 
 int main() {
+    constexpr auto val = my_fun(1, 2, 3, 4, 5);
+
+
+    static_assert(same_as<std::remove_const_t<const int&>, int&>);
+
+    static_assert(same_as<remove_const_t<const int&>, int&>);
+
+    std::tuple<int, double, float> tpl{ 1, 2.4, 3.3f };
 
     static_assert(
         same_as<
