592c10ab5230780ae93447594268087f74c2f367
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-24 03:11:41 +0200

    a

--- example.cpp
+++ example.cpp
@@ -8,6 +8,7 @@
 #include "kaixo/overloads.hpp"
 #include "kaixo/list_comprehension.hpp"
 #include "kaixo/range.hpp"
+#include "kaixo/zipped_range.hpp"
 
 using namespace kaixo;
 using namespace kaixo::operators;
@@ -18,16 +19,18 @@ using namespace kaixo::default_variables;
 struct aaa {
     int a;
     int b;
+    int c;
+    int d;
 };
 
 int main() {
 
-    std::vector<aaa> vals{ { 1, 2 }, { 3, 4 }, { 5, 6 } };
+    std::vector<aaa> vals{ { 1, 2, 3, 4 }, { 3, 4, 5, 6 }, { 5, 6, 7, 8 } };
 
-    auto lc = ((a, b) | (a, b) <- vals);
-    
-    for (auto [a, b] : lc) {
-        std::cout << "[" << a << ", " << b << "]\n";
+    auto lc = ((a, b, c) | ((a, b, _, _), c) <- (vals, range(0, inf)));
+
+    for (auto [a, b, c] : lc) {
+        std::cout << "[" << a << ", " << b << ", " << c << "]\n";
     }
 
    // constexpr auto aoinf = struct_get_member<aaa, struct_size_v<aaa>>::get<0>(val);
