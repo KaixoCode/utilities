7e8f5a1e34e86a2e0b05acdddd7f81b632756dfb
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-24 19:55:30 +0200

    a

--- example.cpp
+++ example.cpp
@@ -18,31 +18,64 @@ using namespace kaixo::default_variables;
 
 
 struct aaa {
+    std::string b = "aaa[0]";
+
     struct {
-        int a = 1;
-        int b = 2;
+        std::string a = "aaa[1][0]";
+        std::string b = "aaa[1][1]";
 
         struct {
-            int a = 3;
-            int b = 4;
+            std::string a = "aaa[1][2][0]";
+            std::string b = "aaa[1][2][1]";
         } c;
     } a;
-    int b = 5;
-    int c = 6;
-    int d = 7;
+
+    std::string c = "aaa[2]";
+    std::string d = "aaa[3]";
 };
 
+class bbb {
+    std::string a = "bbb[0]";
+    std::string b = "bbb[1]";
+
+public:
+    constexpr bbb() {}
+
+    template <size_t I>
+    auto& get()& {
+        if constexpr (I == 0) return a;
+        else if constexpr (I == 1) return b;
+    }
+
+    template <size_t I>
+    auto const& get() const& {
+        if constexpr (I == 0) return a;
+        else if constexpr (I == 1) return b;
+    }
+
+    template <size_t I>
+    auto&& get()&& {
+        if constexpr (I == 0) return std::move(a);
+        else if constexpr (I == 1) return std::move(b);
+    }
+};
+
+namespace std {
+    template<> struct tuple_size<bbb> : std::integral_constant<std::size_t, 2> {};
+    template<> struct tuple_element<0, bbb> : std::type_identity<std::string> {};
+    template<> struct tuple_element<1, bbb> : std::type_identity<std::string> {};
+}
 
 int main() {
     using namespace std::string_literals;
 
+    std::vector<aaa> avals{ 1ull }; // aaa is an aggregate
+    std::vector<bbb> bvals{ 1ull }; // bbb is a class with structured bindings defined.
 
-    auto oaine = range(1, 10);
-    auto lc = (b | a <- range(1, 10), b <- views::stride(range(1, 10), a));
+    auto lc = ((a, b) | ((_, (a, _, _), _, _), (_, b)) <- (avals, bvals));
 
-
-    for (auto a : lc) {
-        std::cout << a << '\n';
+    for (auto [a, b] : lc) {
+        std::cout << "[" << a << ", " << b << "]\n";
     }
 
     return 0;
