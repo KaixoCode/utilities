a236560246b3e7a4896b72dfecd00f5410016c1e
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-26 16:33:28 +0200

    a

--- example.cpp
+++ example.cpp
@@ -16,7 +16,6 @@ using namespace kaixo::operators;
 using namespace kaixo::overloads;
 using namespace kaixo::default_variables;
 
-
 struct aaa {
     std::string a = "aaa[0]";
 
@@ -66,6 +65,38 @@ namespace std {
     template<> struct tuple_element<1, bbb> : std::type_identity<std::string> {};
 }
 
+namespace kaixo {
+    template<class C, class B, class E>
+    struct iff {
+        using depend = concat_t<depend<C>, depend<B>, depend<E>>;
+
+        C condition;
+        B body;
+        E otherwise;
+
+        template<class Self>
+        constexpr auto evaluate(this Self&& self, is_named_tuple auto& tpl) {
+            constexpr auto c1 = kaixo::depend<decay_t<decltype(kaixo::evaluate(std::forward<Self>(self).condition, tpl))>>::size == 0;
+            constexpr auto c2 = kaixo::depend<decay_t<decltype(kaixo::evaluate(std::forward<Self>(self).body, tpl))>>::size == 0;
+            constexpr auto c3 = kaixo::depend<decay_t<decltype(kaixo::evaluate(std::forward<Self>(self).otherwise, tpl))>>::size == 0;
+            constexpr auto complete = c1 && c2 && c3;
+            if constexpr (complete) {
+                if (bool(kaixo::evaluate(std::forward<Self>(self).condition, tpl)))
+                    return kaixo::evaluate(std::forward<Self>(self).body, tpl);
+                else return kaixo::evaluate(std::forward<Self>(self).otherwise, tpl);
+            }
+            else {
+                return kaixo::iff{
+                    kaixo::evaluate(std::forward<Self>(self).condition, tpl),
+                    kaixo::evaluate(std::forward<Self>(self).body, tpl),
+                    kaixo::evaluate(std::forward<Self>(self).otherwise, tpl),
+                };
+            }
+        }
+    };
+}
+
+
 int main() {
     using namespace std::string_literals;
 
@@ -73,10 +104,15 @@ int main() {
     std::vector<bbb> bvals{ 1ull }; // bbb is a class with structured bindings defined.
 
     auto lc = ((a, b, c) | (x, y) <- (avals, bvals), (_, (a, b, _), _, _) = x, (_, c) = y);
-
+    
     for (auto [a, b, c] : lc) {
         std::cout << "[" << a << ", " << b << ", " << c << "]\n";
     }
+
+    std::vector<std::pair<int, std::string>> vals{
+        { 3, "Fizz" },
+        { 5, "Buzz" },
+    };
 
     return 0;
 }
