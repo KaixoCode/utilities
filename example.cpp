b17184c56ea10b93375a34db9a43991067d8df78
KaixoCode <jeroenzwiers2@gmail.com>
2023-06-29 03:33:50 +0200

    aa

--- example.cpp
+++ example.cpp
@@ -78,109 +78,48 @@ namespace std {
     template<> struct tuple_element<2, MyType> : std::type_identity<std::string> {};
 }
 
+template<kaixo::is_type_trait T, class Arg>
+struct test_trait {
+    Arg arg;
+
+    template<class ...Args>
+    constexpr bool value() const {
+        return T::template value<Args...> == arg;
+    }
+};
+
+template<kaixo::is_type_trait T, class Arg>
+constexpr auto operator==(T, Arg&& val) {
+    return test_trait<T, Arg>{ std::forward<Arg>(val) };
+}
+
+using namespace kaixo;
 
 int main() {
-    using namespace std::string_literals;
-    using namespace kaixo;
-    using kaixo::tuples::take;
-    using kaixo::tuples::get;
-    using kaixo::tuples::drop;
-    using kaixo::tuples::drop_last;
-    using kaixo::tuples::last;
-    using kaixo::tuples::erase;
-    using kaixo::tuples::insert;
-    using kaixo::tuples::sub;
-    using kaixo::tuples::swap;
-    using kaixo::tuples::remove;
-    using kaixo::tuples::keep;
-    using kaixo::tuples::keep_raw;
-    using kaixo::tuples::keep_indices;
-    using kaixo::tuples::append;
-    using kaixo::tuples::prepend;
-    using kaixo::tuples::unique;
-    using kaixo::tuples::filter;
-    using kaixo::tuples::call;
-    using kaixo::tuples::head;
-    using kaixo::tuples::transform;
-    using kaixo::tuples::elements;
 
-    std::vector<int> ion;
+    constexpr auto sionr = (sizeof_v<> == 4).template evaluate<uint32_t>();
 
-    std::ranges::filter_view ae{ ion, [](int val) { return val == 0; } };
+    using namespace kaixo::pack;
 
+    //using namespace kaixo::pack;
 
-    auto sorin = [&](auto& val) { return val + 1; };
-
-    info<int&, double&>::filter<!can_invoke<decltype(sorin)>>::size;
-
-    info<int, double, float>::transform<partial<std::invoke_result_t, decltype(sorin)>::type>::size;
-
-    constexpr std::tuple tpl{ 1, 1., 1.f, 1, 1., 1.f };
-
-    decltype(take<2>)::tuple_pipe;
-
-    constexpr auto soinr = invocable<decltype(take<2>), std::tuple<int, int>&>;
-
-    constexpr auto isonr = tuples::pipe<decltype(take<2>), std::tuple<int, int>&>;
-
-
-    auto sorin = tpl | take<5> | drop_last<1> | last<3> | erase<1>;
-
-    int af = 1;
-
-    constexpr auto soinsr = std::tuple{ 1, 1., 1.f, 1, 1., 1.f }
-        | drop<2> | last<4> | erase<0> | swap<0>(1.) | keep_indices<0, 1, 2> 
-        | sub<0, 1> | append(1, 1., 1.f) | prepend(1) | unique | filter<is_integral || is_floating_point>;
-
-    constexpr auto soinrs = soinsr | transform([]<class Arg>(Arg&& arg) { return arg + 1; });
-
-    constexpr auto sroinfg = call(soinsr, [](auto& ...args) { return (args + ...); });
-
-    decltype(soinsr)::types::size;
-
-    constexpr auto v1 = soinrs.get<0>();
-    constexpr auto v2 = soinrs.get<1>();
-    constexpr auto v3 = soinrs.get<2>();
-
-    constexpr auto sorignsr = std::tuple{
-        std::tuple{ 1, 2., 3.f },
-        std::tuple{ 1., 2.f, 3 },
-        std::tuple{ 1.f, 2, 3. },
-    } | elements<0>;
-
-    constexpr auto q1 = sorignsr.get<0>();
-    constexpr auto q2 = sorignsr.get<1>();
-    constexpr auto q3 = sorignsr.get<2>();
-
-    //auto oisnr = tuples::all(soinsr);
-
-    //auto oignr = insert<0>(sorin, 1, 2, 3);
-
-    //using oinsr = tuples::all_t<decltype(sorin)>;
-
-    //auto rsoni = sorin | insert<0>(1, 2, 3);
-
-    //decltype(rsoni)::types::size;
-    
-    //rsoni.get<0>();
-    //rsoni.get<1>();
-
-    //auto rsgr1 = std::get<0>(std::move(resr));
-    //auto rsgr2 = std::get<1>(resr);
-
-    auto re = kaixo::tuples::all(std::move(tpl));
-
-    auto fe = kaixo::tuples::all(re);
-
-    //std::tuple res = tpl
-    //    | append("Hello World"s, 3.)
-    //    | prepend(0.)
-    //    | reverse
-    //    | remove_indices<2, 3>
-    //    | take<3>
-    //    | drop<1>;
+    //kaixo::info<kaixo::info<char, int>, kaixo::info<float, double>>::filter<contains_all_v<char, int>>::size;
     //
-    //std::tuple res2 = zip(res, tpl);
+    //contains_v<int>.value<std::tuple<int, double>>;
+    //
+    //contains_all<kaixo::info<int, double>, kaixo::info<int, double, float>>::value;
+    //
+    //contains_all<kaixo::info<int>>::value;
+    //
+    //kaixo::info<kaixo::info<char, int>, kaixo::info<float, double>>::filter<count_v<int> == 1>::size;
+    //
+    //
+    //kaixo::info<int, double, char, long>::filter<(contains_v<int> > 2)>::size;
+    //
+    //kaixo::sizeof_v<> == 2;
+    //
+    //kaixo::sizeof_v<>;
+    //element_t<0>;
 
     return 0;
 }
