fdf61db48e19c31e6222fa9f26db7badb6a67a69
KaixoCode <jeroenzwiers2@gmail.com>
2023-08-07 22:52:19 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -11,24 +11,14 @@
 
 
 
-#include "kaixo/list_comprehension.hpp"
-#include "kaixo/overloads.hpp"
-#include "kaixo/range.hpp"
-#include "kaixo/break.hpp"
-#include "kaixo/zipped_range.hpp"
 
 
 
 
 int main() {
-    using namespace kaixo;
-    using namespace kaixo::operators;
-    using namespace kaixo::default_variables;
 
-    auto r1 = ((a, b, c) | c <- range(1, 11), b <- range(1, c), a <- range(1, b), a * a + b * b == c * c);
 
-    for (auto [a, b, c] : r1) {
-        std::cout << "[" << a << ", " << b << ", " << c << "]\n";
-    }
+
+
 
 }
